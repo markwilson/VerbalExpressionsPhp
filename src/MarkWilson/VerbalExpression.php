b5288937946950c18d248232f3f8d10d76547736
Mark Wilson <mwilson@rippleffect.com>
2013-07-24 17:01:15 +0100

    Start adding sub pattern option - tests are broken

--- src/MarkWilson/VerbalExpression.php
+++ src/MarkWilson/VerbalExpression.php
@@ -36,27 +36,36 @@ class VerbalExpression
      * @var string
      */
     private $modifiers = '';
+    /**
+     * Use sub patterns
+     *
+     * @var boolean
+     */
+    private $subPattern = true;
 
     /**
      * Match anything
      *
+     * @param boolean $subPattern Collect as a sub pattern
+     *
      * @return $this
      */
-    public function anything()
+    public function anything($subPattern = null)
     {
-        return $this->add('(.*)');
+        return $this->add('.*', $subPattern);
     }
 
     /**
      * Match anything except characters in $value
      *
-     * @param string $value String of characters to ignore
+     * @param string  $value      String of characters to ignore
+     * @param boolean $subPattern Collect as a sub pattern
      *
      * @return $this
      */
-    public function anythingBut($value)
+    public function anythingBut($value, $subPattern = null)
     {
-        return $this->add('([^' . $this->sanitise($value) . ']*)');
+        return $this->add('[^' . $this->sanitise($value) . ']*)', $subPattern);
     }
 
     /**
@@ -76,25 +85,27 @@ class VerbalExpression
     /**
      * Shorthand for then method
      *
-     * @param string $value String to find
+     * @param string  $value String to find
+     * @param boolean $subPattern Collect as a sub pattern
      *
      * @return $this
      */
-    public function find($value)
+    public function find($value, $subPattern = null)
     {
-        return $this->then($value);
+        return $this->then($value, $subPattern);
     }
 
     /**
      * Add value or not
      *
-     * @param string $value Maybe value
+     * @param string  $value Maybe value
+     * @param boolean $subPattern Collect as a sub pattern
      *
      * @return $this
      */
-    public function maybe($value)
+    public function maybe($value, $subPattern = null)
     {
-        return $this->add('(' . $this->sanitise($value) . ')?');
+        return $this->add($this->sanitise($value), $subPattern, '?');
     }
 
     /**
@@ -114,67 +125,84 @@ class VerbalExpression
     /**
      * Add a sanitised value check
      *
-     * @param string $value Value to sanitise
+     * @param string  $value Value to sanitise
+     * @param boolean $subPattern Collect as a sub pattern
      *
      * @return $this
      */
-    public function then($value)
+    public function then($value, $subPattern = null)
     {
-        return $this->add('(' . $this->sanitise($value) . ')');
+        return $this->add($this->sanitise($value), $subPattern);
     }
 
     /**
      * Shorthand for anyOf method
      *
-     * @param string $value String of characters
+     * @param string  $value String of characters
+     * @param boolean $subPattern Collect as a sub pattern
      *
      * @return $this
      */
-    public function any($value)
+    public function any($value, $subPattern = null)
     {
-        return $this->anyOf($value);
+        return $this->anyOf($value, $subPattern);
     }
 
     /**
      * Any of the specified characters
      *
-     * @param string $value String of characters
+     * @param string  $value String of characters
+     * @param boolean $subPattern Collect as a sub pattern
      *
      * @return $this
      */
-    public function anyOf($value)
+    public function anyOf($value, $subPattern = null)
     {
-        return $this->add('[' . $this->sanitise($value) . ']');
+        return $this->add('[' . $this->sanitise($value) . ']', $subPattern);
     }
 
     /**
      * Shorthand for lineBreak method
      *
+     * @param boolean $subPattern Collect as a sub pattern
+     *
      * @return $this
      */
-    public function br()
+    public function br($subPattern = null)
     {
-        return $this->lineBreak();
+        return $this->lineBreak($subPattern);
     }
 
     /**
      * Search for new line
      *
-     * @return $this
-     */
-    public function lineBreak()
-    {
-        return $this->add("(\\n|(\\r\\n))");
-    }
-
-    /**
-     * Characters in range
+     * @param boolean $subPattern Collect as a sub pattern
      *
      * @return $this
      */
-    public function range()
+    public function lineBreak($subPattern = null)
+    {
+        return $this->add("\\n|(?:\\r\\n)", $subPattern);
+    }
+
+    /**
+     * Characters in range - requires pairs of arguments, provide subPattern as final argument
+     *
+     * @param boolean $subPattern Collect as a sub pattern
+     *
+     * @return $this
+     */
+    public function range($subPattern = null)
     {
         $arguments = func_get_args();
+
+        // odd number of arguments, must assume last is subPattern
+        if (count($arguments) % 2 === 1) {
+            $subPattern = array_pop($arguments);
+        } else {
+            $subPattern = null;
+        }
+
         $value     = '[';
 
         for ($fromIndex = 0; $fromIndex < count($arguments); $fromIndex += 2) {
@@ -191,27 +219,31 @@ class VerbalExpression
 
         $value .= ']';
 
-        return $this->add($value);
+        return $this->add($value, $subPattern);
     }
 
     /**
      * Add tab character
      *
+     * @param boolean $subPattern Collect as a sub pattern
+     *
      * @return $this
      */
-    public function tab()
+    public function tab($subPattern = null)
     {
-        return $this->add("\\t");
+        return $this->add("\\t", $subPattern);
     }
 
     /**
      * Match entire word
      *
+     * @param boolean $subPattern Collect as a sub pattern
+     *
      * @return $this
      */
-    public function word()
+    public function word($subPattern = null)
     {
-        return $this->add("\\w+");
+        return $this->add("\\w+", $subPattern);
     }
 
     /**
@@ -253,13 +285,15 @@ class VerbalExpression
     /**
      * Add a regular expression partial
      *
-     * @param string $expression Regular expression partial to add
+     * @param string  $expression           Regular expression partial to add
+     * @param boolean $subPattern           Collect as a sub pattern
+     * @param string  $additionalCharacters Additional characters after brackets - e.g. ?
      *
      * @return $this
      */
-    public function add($expression)
+    public function add($expression, $subPattern = null, $additionalCharacters = '')
     {
-        $this->expression .= $expression;
+        $this->expression .= $this->addBrackets($expression, $subPattern) . $additionalCharacters;
 
         return $this;
     }
@@ -296,14 +330,15 @@ class VerbalExpression
     public function orPipe($value)
     {
         if (false === strpos($this->prefixes, '(')) {
-            $this->prefixes .= '(';
+            $this->prefixes .= $this->generateOpeningBracket();
         }
 
         if (false === strpos($this->suffixes, ')')) {
             $this->suffixes = ')' . $this->suffixes;
         }
 
-        $this->add(')|(');
+        $this->add(')|' . $this->generateOpeningBracket());
+
         return $this->then($value);
     }
 
@@ -408,5 +443,34 @@ class VerbalExpression
     private function hasModifier($modifier)
     {
         return strpos($this->modifiers, $modifier) !== false;
+    }
+
+    /**
+     * Wrap string in brackets
+     *
+     * @param string  $text       Text to wrap
+     * @param boolean $subPattern Use sub patterns
+     *
+     * @return string
+     */
+    private function addBrackets($text, $subPattern = null)
+    {
+        return $this->generateOpeningBracket($subPattern) .  $text . ')';
+    }
+
+    /**
+     * Generate the opening bracket
+     *
+     * @param boolean $subPattern Use sub patterns?
+     *
+     * @return string
+     */
+    private function generateOpeningBracket($subPattern = null)
+    {
+        if ($subPattern !== true && (false === $subPattern || false === $this->subPattern)) {
+            return '(?:';
+        }
+
+        return '(';
     }
 }
